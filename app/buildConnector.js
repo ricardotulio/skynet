b20ebb81d072798527e150728cbdd7607a1c581e
Ricardo Ledo <ledo.tulio@gmail.com>
2018-03-06 23:57:08 +0000

    chore: refact bot structure
    Create tests to bot functions and rename its. Also create a directory
    named `botBuild` to contains associated with bot construction.

--- app/buildConnector.js
+++
@@ -1,8 +0,0 @@
-import { ChatConnector } from 'botbuilder'
-
-const buildConnector = () => new ChatConnector({
-  appId: process.env.MICROSOFT_APP_ID,
-  appPassword: process.env.MICROSOFT_APP_PASSWORD,
-})
-
-export default buildConnector
