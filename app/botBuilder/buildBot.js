f3b3c7b6a7a7d59fa174d698f1240d288e84fd22
Ricardo Ledo <ledo.tulio@gmail.com>
2018-03-24 14:51:25 +0000

    bot: define a storage method
    The bot persist some data about users. This data is responsible for
    identify the user and is used to define who will receive an asware, for
    example, but default storage method was deprecated, so bot stop to answare.
    
    To solve this problem, this commit set inMemoryStorage as a storage method.

--- app/botBuilder/buildBot.js
+++ app/botBuilder/buildBot.js
@@ -1,7 +1,12 @@
-import { UniversalBot } from 'botbuilder'
+import {
+  MemoryBotStorage,
+  UniversalBot,
+} from 'botbuilder'
+
+const inMemoryStorage = new MemoryBotStorage()
 
 const buildBot = connector => new UniversalBot(connector, (session) => {
   session.send('You said %s', session.message.text)
-})
+}).set('storage', inMemoryStorage)
 
 export default buildBot
